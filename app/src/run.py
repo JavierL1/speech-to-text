a5e16585e47cfbbe1c10603c1b6441d4fc574e29
Javier Levio <javier.levio.12@sansano.usm.cl>
2021-05-29 01:40:10 -0400

    Added functionality to translate all video files in a folder

--- app/src/run.py
+++ app/src/run.py
@@ -6,8 +6,7 @@ from core.extract_audio import extract_and_save_audio
 from core.speech_to_text import write_large_audio_transcription
 from core.utils import seconds_to_human
 
-if __name__ == '__main__':
-    video_filename = input('Ingrese nombre del video. Ejemplo: video.mp4\n')
+def translate_one_video_file(video_filename: str):
     start_time = time.time()
     video_filepath = Path('/data', video_filename)
     filename = video_filepath.with_suffix('').stem
@@ -17,3 +16,14 @@ if __name__ == '__main__':
     write_large_audio_transcription(audio_path, text_path)
     elapsed_time_seconds = int(time.time() - start_time)
     print(f'Tiempo total de la ejecución: {seconds_to_human(elapsed_time_seconds)}')
+
+if __name__ == '__main__':
+    user_input = str(input('Ingrese nombre del video a traducir o escriba "todo" para traducir todos los videos en la carpeta. Ejemplo: video.mp4\n'))
+    if user_input == 'todo':
+        data_path = Path('/data')
+        video_filenames = [p.name for p in data_path.iterdir() if p.name.endswith('.mp4')]
+        print(f'Beginning to translate these videos: {video_filenames}')
+        for filename in video_filenames:
+            translate_one_video_file(filename)
+    else:
+        translate_one_video_file(user_input)
